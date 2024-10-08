7b4eff94d22a71a3a576c63332e936d5d73b432e
Rimon Hossain <rimonhossen333@gmail.com>
2024-10-08 17:11:45 +0600

    add backend link

--- client/src/api/serverApi.js
+++ client/src/api/serverApi.js
@@ -1,5 +1,5 @@
-// export const backendDomain = "https://client-work-6t1v.onrender.com";
-export const backendDomain = "http://localhost:5000";
+export const backendDomain = "https://client-work-6t1v.onrender.com";
+// export const backendDomain = "http://localhost:5000";
 const ServerApi = {
   signUp: {
     url: `${backendDomain}/user/register`,
